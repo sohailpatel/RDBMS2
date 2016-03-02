a9eb14eedde8b45a5832080d588715b9a4ed1d80
sohailpatel <sohailpatelt20@gmail.com>
2016-03-02 11:04:54 +0530

    Procedure with Cursor added to find total revenew obtained from product

--- procedure.sql
+++ procedure.sql
@@ -1,1 +1,7 @@
-create procedure totalcost(in checkid int)  begin  declare total float default 0;  declare cc float default 0; declare cost_cur cursor for select cost from orders where productid=checkid; open cost_cur; loop  fetch from cost_cur into cc;set total = cc+total; update display set total_sell=total where id=0; select total_sell from display; end loop; close cost_cur; end;//
+delimiter //
+
+create procedure displaycost() begin select productid,total_sell from display where id=0;end;//
+
+
+create procedure totalcost(in checkid int)  begin  declare total float default 0;  declare cc float default 0; declare done int default 0;declare cost_cur cursor for select cost from orders where productid=checkid;DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;  open cost_cur; cursorfetch:loop fetch from cost_cur into cc;if done=0 then set total = cc+total; update display set total_sell=total where id=0; update display set productid=checkid where id=0; else call displaycost(); leave cursorfetch; end if;end loop cursorfetch; close cost_cur; set done=0;end;//
+
