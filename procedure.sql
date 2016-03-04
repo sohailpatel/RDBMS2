ff1c2b8da95773967643f4beea563070d3c91c38
sohailpatel <sohailpatelt20@gmail.com>
2016-03-04 10:30:16 +0530

    Cursor within Procedure for getting profit of the product

--- procedure.sql
+++ procedure.sql
@@ -1,6 +1,9 @@
 delimiter //
 
-create procedure displaycost() begin select productid,total_sell as Total_Revenew_Earned,total_quantity as Toal_Products_Sold from display where id=0;end;//
+create procedure displaycost() 
+begin 
+select productid,total_sell as Total_Revenew_Earned,total_quantity as Toal_Products_Sold from display where id=0;
+end;//
 
 
 
@@ -16,23 +19,28 @@ declare done int default 0;
 declare tpr float default 0;
 declare cost_cur cursor for select cost,quantity from orders where productid=checkid; 
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;   
-block2: begin
-declare pr cursor for select profit from productname where id=checkid;
-open pr; 
-curloop:loop fetch from pr into tpr;
-leave curloop;close pr;
-end loop curloop;end block2;  
+block2: 
+  begin
+  declare pr cursor for select profit from productname where id=checkid;
+  open pr; 
+  curloop:loop 
+    fetch from pr into tpr;
+    leave curloop;
+    close pr;
+  end loop curloop;
+end block2;  
 open cost_cur;  
 cursorfetch:loop  
-fetch from cost_cur into cc,quan; 
-if done=0 then 
-set total1 = tpr*quan; 
-set total=total+total1;
-set totalquan=quan+totalquan;  
-else   
-update display set productid=checkid,total_quantity=totalquan,total_sell=total where id=0;   
-call displaycost();  
-leave cursorfetch;  
-end if; 
+  fetch from cost_cur into cc,quan; 
+  if done=0 then 
+    set total1 = tpr*quan; 
+    set total=total+total1;
+    set totalquan=quan+totalquan;  
+  else   
+    update display set productid=checkid,total_quantity=totalquan,total_sell=total where id=0;   
+    call displaycost();  
+    leave cursorfetch;  
+  end if; 
 end loop cursorfetch;  
-close cost_cur;end;//
+close cost_cur;
+end;//
